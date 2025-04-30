164ad9a826d6b353681db42db0d9a8572254a906
Yiwang5 <yiwang5@s.sfusd.edu>
2025-04-30 09:47:24 -0700

    Update APCalendar.java

--- src/main/java/APCalendar.java
+++ src/main/java/APCalendar.java
@@ -55,6 +55,6 @@ public class APCalendar
    */
   public static int dayOfWeek(int month, int day, int year)
   {
-    /* to be implemented in part (b) */
+    return (firstDayOfYear(year) + DayOfYear(month, day, year) -1 ) % 7;
   }
 }
